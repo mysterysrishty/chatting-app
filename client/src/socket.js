21cf0a51c86b8ebfe100bb3a155b96bbeb7c2f34
mysterysrishty <148486320+mysterysrishty@users.noreply.github.com>
2026-03-30 00:20:05 +0530

    fix socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+import { io } from "socket.io-client";
+
+const socket = io("https://srishty-social-backend.onrender.com");
+
+export default socket;
